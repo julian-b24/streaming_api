cdf0b4f4a389adc7ce24f13ca0ff2ef4936d7b55
Julian Bolaños <julian.camilob24@gmail.com>
2024-09-20 14:59:13 -0500

    Springboot initlzr template added

---
+++ src/test/java/com/stream/streaming_api/StreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.streaming_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
