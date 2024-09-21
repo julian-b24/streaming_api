9eec338a21b26e070b255308b36371889c31c6fc
Julian Bolaños <julian.camilob24@gmail.com>
2024-09-20 21:59:40 -0500

    feat (env): Added enviroment variables by .env file

--- src/main/java/com/stream/streaming_api/StreamingApiApplication.java
+++ src/main/java/com/stream/streaming_api/StreamingApiApplication.java
@@ -2,8 +2,10 @@ package com.stream.streaming_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class StreamingApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/stream/streaming_api/api/StreamingAPI.java
+++ src/main/java/com/stream/streaming_api/api/StreamingAPI.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/streaming")
 public interface StreamingAPI {
 
